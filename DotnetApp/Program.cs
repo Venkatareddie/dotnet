323972b5be92096b74d7357bbfc1fdbcb6ddf709
Venkatareddie <84559316+Venkatareddie@users.noreply.github.com>
2025-04-22 16:18:59 -0400

    Update Program.cs

--- DotnetApp/Program.cs
+++ DotnetApp/Program.cs
@@ -97,7 +97,7 @@ class Program
         }
         */
 
-        /*
+        /* Matrix Addition - Write a program to add two 3x3 matrices and display the resulting matrix.
         int [,] a = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         int[,] b = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
         int[,] sum = new int[3,3];
